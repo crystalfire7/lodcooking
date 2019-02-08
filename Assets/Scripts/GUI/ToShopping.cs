169162044bf533497c88efd5139cc38c6db3b2a6
danielw <daniel.wong@utexas.edu>
2019-02-08 11:49:19 -0600

    added shopping button

---
+++ Assets/Scripts/GUI/ToShopping.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.SceneManagement;
+
+public class ToShopping : MonoBehaviour
+{
+    Button button;
+    // Start is called before the first frame update
+    void Start()
+    {
+        button = GetComponent<Button>();
+        button.onClick.AddListener(ToShop);   
+    }
+
+    // Update is called once per frame
+    void ToShop()
+    {
+        SceneManager.LoadScene("Shopping");
+    }
+}
