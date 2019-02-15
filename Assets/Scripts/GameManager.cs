62830817efc283ac5a5771c1ee43ddc47f7fe772
crystalfire7 <12specialreindeers@gmail.com>
2019-02-15 16:21:11 -0600

    added GameManager

---
+++ Assets/Scripts/GameManager.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GameManager : MonoBehaviour {
+	private static GameManager instance;
+	public static GameManager Instance {
+		get {
+			return instance;
+		}
+	}
+
+	Grill grill;
+
+	void Awake () {
+		grill = GameObject.FindGameObjectWithTag("Grill").GetComponent<Grill>();
+	}
+
+	public Grill GrillObj {
+		get {
+			return grill;
+		}
+	}
+
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}
