52895f104b3f1d6912335ea1e1461d80c79de900
crystalfire7 <12specialreindeers@gmail.com>
2019-02-18 23:57:31 -0600

    updated start screen

---
+++ Assets/Scripts/GUI/ToPizza.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+using HTC.UnityPlugin.ColliderEvent;
+using HTC.UnityPlugin.Utility;
+
+public class ToPizza : MonoBehaviour, IColliderEventHoverEnterHandler
+{
+    public void OnColliderEventHoverEnter(ColliderHoverEventData eventData)
+    {
+        SceneManager.LoadScene("Shopping");
+    }
+}
