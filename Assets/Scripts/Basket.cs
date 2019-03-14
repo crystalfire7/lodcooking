9d56a06c778b8f2925264245af10ef8cf2c4c406
omar <aa78749@yahoo.com>
2019-03-14 17:50:55 -0500

    push bag changes

---
+++ Assets/Scripts/Basket.cs
@@ -0,0 +1,45 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class Basket : MonoBehaviour
+{
+    public List<string> inBag;
+    public List<string> needed;
+    public bool isDone;
+
+    private void Start()
+    {
+        isDone = false;
+    }
+
+    private void Update()
+    {
+        List<string> temp = new List<string>(needed);
+        foreach (string item in inBag)
+        {
+            temp.Remove(item);
+        }
+        if (temp.Count == 0)
+        {
+            isDone = true;
+        }
+    }
+    void OnTriggerEnter(Collider other)
+    {
+        string name = LayerMask.LayerToName(other.gameObject.layer);
+        if (name == "Food")
+        {
+            inBag.Add(other.tag);
+        }
+    }
+    private void OnTriggerExit(Collider other)
+    {
+        string name = LayerMask.LayerToName(other.gameObject.layer);
+        if (name == "Food")
+        {
+            inBag.Remove(other.tag);
+        }
+    }
+}
