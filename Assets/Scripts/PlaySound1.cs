fe5a7bb85501f448ebda5f976b9d12edb8a1d155
omar <aa78749@yahoo.com>
2019-03-14 20:16:29 -0500

    Sound Changes

---
+++ Assets/Scripts/PlaySound1.cs
@@ -0,0 +1,28 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class PlaySound1 : MonoBehaviour
+{
+
+    public AudioClip SoundToPlay;
+    public float Volume;
+    AudioSource audio;
+    public bool alreadyPlayed = false;
+    void Start()
+    {
+        audio = GetComponent<AudioSource>();
+    }
+
+    void OnTriggerEnter(Collider other)
+    {
+        if (other.tag == "Grill")
+        {
+            Debug.Log("intersect");
+            if (!alreadyPlayed)
+            {
+                audio.PlayOneShot(SoundToPlay, Volume);
+                alreadyPlayed = true;
+            }
+        }
+    }
+}
